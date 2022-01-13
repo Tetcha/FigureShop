aef1a1bdc9ae1ee462d771455da6a844add5baaa
Duc Dau <dauleduc2@gmail.com>
2022-01-13 21:53:38 +0700

    UPDATE - add name field into filter

---
+++ FigureShop/web/asset/js/filter.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+  const fromElement = $("#fromFilter");
+  const toElement = $("#toFilter");
+  const nameElement = $("#nameFilter");
+  let selectedVal = "";
+  $("#categoryFilter").change(function (e) {
+    e.preventDefault();
+    selectedVal = $("#categoryFilter option:selected").val();
+  });
+  const baseUrl = "http://localhost:8080/FigureShop";
+  $("#filterSearchButton").click(function (e) {
+    e.preventDefault();
+
+    let fromValue = fromElement.val();
+    if (!fromValue) fromValue = "0";
+
+    let toValue = toElement.val();
+    if (!toValue) toValue = "9999999999";
+
+    const nameValue = nameElement.val();
+
+    window.location.href = `${baseUrl}/filter?name=${nameValue}&from=${fromValue}&to=${toValue}&categoryId=${selectedVal}&page=${1}`;
+  });
+});
