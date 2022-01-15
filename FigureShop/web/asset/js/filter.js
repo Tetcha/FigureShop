e1b9f20682801267c24266cc942509e72d77eb9b
Duc Dau <dauleduc2@gmail.com>
2022-01-15 16:44:43 +0700

    UPDATE - save value on filter

--- FigureShop/web/asset/js/filter.js
+++ FigureShop/web/asset/js/filter.js
@@ -4,6 +4,30 @@ $(document).ready(function () {
   const toElement = $("#toFilter");
   const toText = $("#toFilterText");
   const nameElement = $("#nameFilter");
+  const paramValue = {
+    name: "",
+    from: "",
+    to: "",
+    categoryId: "",
+    page: "",
+  };
+  const paramQueryList = window.location.href.split("?")[1].split("&");
+
+  paramQueryList.forEach((param) => {
+    const paramSplitList = param.split("=");
+    if (param[1] != "") {
+      paramValue[paramSplitList[0]] = paramSplitList[1];
+    }
+  });
+  //set default value for each field
+  nameElement.val(paramValue.name);
+  fromElement.val(paramValue.from);
+  toElement.val(paramValue.to);
+  console.log($(`#categoryFilter option[value='${paramValue.categoryId}']`));
+  $(`#categoryFilter option[value='${paramValue.categoryId}']`).attr(
+    "selected",
+    "selected"
+  );
 
   let selectedVal = "";
   $("#categoryFilter").change(function (e) {
@@ -11,12 +35,13 @@ $(document).ready(function () {
     selectedVal = $("#categoryFilter option:selected").val();
   });
   const baseUrl = "http://localhost:8080/FigureShop";
+
   $("#filterSearchButton").click(function (e) {
     e.preventDefault();
     const canMove = true;
     let fromValue = fromElement.val();
     let toValue = toElement.val();
-
+    const nameValue = nameElement.val();
     if (isNaN(fromValue) || parseInt(fromValue) < 0) {
       fromText.append("From value should be a valid positive number");
       canMove = false;
@@ -30,7 +55,6 @@ $(document).ready(function () {
     }
     if (!toValue) toValue = "99999999";
     if (!fromValue) fromValue = "0";
-    const nameValue = nameElement.val();
 
     if (canMove)
       window.location.href = `${baseUrl}/filter?name=${nameValue}&from=${fromValue}&to=${toValue}&categoryId=${selectedVal}&page=${1}`;
